be038b1d79a4b594508664e2757adf05a02862f6
Andy Albrecht <aalbrecht33733@gmail.com>
2022-02-14 14:02:16 -0500

    Re-organize and added exercises to quiz solutions

--- js/quizSolutions.js
+++ js/quizSolutions.js
@@ -13,154 +13,154 @@
 //         }
 //     }
 // }
-//////////////////////////////////////////////////////////
 
 
-// const mystery = 'Pageof7'; //CHANGE THIS VALUE TO MAKE THE CONDITIONAL BELOW TRUE
+///////////////////////////////////////
+///CHANGE THIS VALUE TO MAKE THE CONDITIONAL BELOW TRUE:
+const mystery = 'Pageof7';
+
+/// LEAVE THIS CODE ALONE! (pretty please)
+if (mystery[0] === 'P' && mystery.length > 5 && mystery.indexOf('7') !== -1) {
+    console.log("YOU GOT IT!!!");
+}
 
 
-// // LEAVE THIS CODE ALONE! (pretty please)
-// if (mystery[0] === 'P' && mystery.length > 5 && mystery.indexOf('7') !== -1) {
-//     console.log("YOU GOT IT!!!");
-// }
-
-////////////////////////////////////////////////////////
-
+///////////////////////////////////////
 // DO NOT TOUCH!!! (please)
-// const airplaneSeats = [
-//     ['Ruth', 'Anthony', 'Stevie'],
-//     ['Amelia', 'Pedro', 'Maya'],
-//     ['Xavier', 'Ananya', 'Luis'],
-//     ['Luke', null, 'Deniz'],
-//     ['Rin', 'Sakura', 'Francisco']
-// ];
+const airplaneSeats = [
+    ['Ruth', 'Anthony', 'Stevie'],
+    ['Amelia', 'Pedro', 'Maya'],
+    ['Xavier', 'Ananya', 'Luis'],
+    ['Luke', null, 'Deniz'],
+    ['Rin', 'Sakura', 'Francisco']
+];
 
 // // YOUR CODE GOES BELOW THIS LINE:
-''
+airplaneSeats[3].splice(1, 1, 'Hugo');
 
-// airplaneSeats[3].splice(1, 1, 'Hugo');
 
-/////////////////////////////////////////////////////////
-
+///////////////////////////////////////
 //PLEASE DON'T TOUCH THIS LINE!
-// const restaurant = {
-//     name: 'Ichiran Ramen',
-//     address: `${Math.floor(Math.random() * 100) + 1} Johnson Ave`,
-//     city: 'Brooklyn',
-//     state: 'NY',
-//     zipcode: '11206',
-// }
+const restaurant = {
+    name: 'Ichiran Ramen',
+    address: `${Math.floor(Math.random() * 100) + 1} Johnson Ave`,
+    city: 'Brooklyn',
+    state: 'NY',
+    zipcode: '11206',
+}
 
-// //YOUR CODE GOES DOWN HERE:
+//YOUR CODE GOES DOWN HERE:
+const fullAddress = restaurant.address + ', ' + restaurant.city + ', ' + restaurant.state + ', ' + restaurant.zipcode
 
-// const fullAddress = restaurant.address + ', ' + restaurant.city + ', ' + restaurant.state + ', ' + restaurant.zipcode
 
-///////////////////////////////////////////////////////
+////////////////////////////////////////
+for (let i = 0; i < 13; i++) {
+    console.log("Thirteen Son!")
+}
 
-// for (let i = 0; i < 13; i++) {
-//     console.log("Thirteen Son!")
-// }
+/// Print out "Da ba dee da ba daa" 6 times,
+/// using a for loop
+for (let i = 0; i < 6; i++) {
+    console.log("Da ba dee da ba daa")
+}
 
-// Print out "Da ba dee da ba daa" 6 times, using a for loop
-// for (let i = 0; i < 6; i++) {
-//     console.log("Da ba dee da ba daa")
-// }
 
-///////////////////////////////////////////////////////
-
-// Write a loop that prints:
+/////////////////////////////////////////
+/// Write a loop that prints:
 // 25
 // 20
 // 15
 // 10
 // 5
 // 0
-// for (let i = 25; i >= 0; i -= 5) {
-//     console.log(i);
-// }
+for (let i = 25; i >= 0; i -= 5) {
+    console.log(i);
+}
 
-///////////////////////////////////////////////////////
 
-// const people = ["Scooby", "Velma", "Daphne", "Shaggy", "Fred"]; //DONT TOUCH THIS LINE!
+/////////////////////////////////////////
+/// DONT TOUCH THIS LINE!
+const people = ["Scooby", "Velma", "Daphne", "Shaggy", "Fred"]; 
 
-// // WRITE YOUR  for LOOP BELOW THIS LINE:
-// for (let i = 0; i < people.length; i++) {
-//     console.log(people[i].toUpperCase());
-// }
+/// WRITE YOUR  for LOOP BELOW THIS LINE:
+for (let i = 0; i < people.length; i++) {
+    console.log(people[i].toUpperCase());
+}
 
-/////////////////////////////////////////////////////
 
-// const seatingChart = [
-//     ['Kristen', 'Eric', 'Namita'],
-//     ['Geoffrey', 'Juanita', 'Antonio', 'Kevin'],
-//     ['Yuma', 'Sakura', 'Jack', 'Erika']
-// ]
+//////////////////////////////////////
+const seatingChart = [
+    ['Kristen', 'Eric', 'Namita'],
+    ['Geoffrey', 'Juanita', 'Antonio', 'Kevin'],
+    ['Yuma', 'Sakura', 'Jack', 'Erika']
+]
 
-// for (let i = 0; i < seatingChart.length; i++) {
-//     const row = seatingChart[i];
-//     console.log(`ROW #${i + 1}-------------`);
-//     for (let j = 0; j < row.length; j++)
-//     console.log(row[j])
-// }
+for (let i = 0; i < seatingChart.length; i++) {
+    const row = seatingChart[i];
+    console.log(`ROW #${i + 1}-------------`);
+    for (let j = 0; j < row.length; j++)
+    console.log(row[j])
+}
 
-//////////////////////////////////////////////////////
-/// Exercise
 
-// function rant(message) {
-//     console.log(message.toUpperCase());
-//     console.log(message.toUpperCase());
-//     console.log(message.toUpperCase());
-// }
-// rant("I hate beets");
+//////////////////////////////////////
+/// Exercise:
+function rant(message) {
+    console.log(message.toUpperCase());
+    console.log(message.toUpperCase());
+    console.log(message.toUpperCase());
+}
+rant("I hate beets");
 
-// rant("Fuck Yeah Mann!");
+rant("Fuck Yeah Mann!");
 
-//////////////////////////////////////////////////////
 
-// function isSnakeEyes(num1, num2) {
-//     if(num1 === 1 && num2 === 1) {
-//         console.log("Snake Eyes!");
-//     } else {
-//         console.log("Not Snake Eyes!");
-//     }
-// }
+//////////////////////////////////////
+function isSnakeEyes(num1, num2) {
+    if(num1 === 1 && num2 === 1) {
+        console.log("Snake Eyes!");
+    } else {
+        console.log("Not Snake Eyes!");
+    }
+}
 
-// isSnakeEyes(2, 3);
-// isSnakeEyes(1, 1);
+isSnakeEyes(2, 3);
+isSnakeEyes(1, 1);
 
-///////////////////////////////////////////////////////
 
-// DEFINE YOUR FUNCTION BELOW:
-// function multiply(num1, num2) {
-//     return (num1 * num2);
-// }
+/////////////////////////////////////
+/// DEFINE YOUR FUNCTION BELOW:
+function multiply(num1, num2) {
+    return (num1 * num2);
+}
 
-// multiply(2, 3);
+multiply(2, 3);
 
-///////////////////////////////////////////////////
 
-// function isShortsWeather(temp) {
-//     if (temp > 75) {
-//         return true;
-//     }
-//     return false;
-// }
+////////////////////////////////////
+function isShortsWeather(temp) {
+    if (temp > 75) {
+        return true;
+    }
+    return false;
+}
 
-// isShortsWeather(80);
+isShortsWeather(80);
 
-////////////////////////////////////////////////
 
+/////////////////////////////////////////
 // function lastElement(arr) {
 //     return arr.length - 1
 // }
 
-///////////////////////////////////////////
 
+/////////////////////////////////////////
 // const square = function (num) {
 //     return num * num;
 // }
 
-//////////////////////////////////////////
+
+/////////////////////////////////////////
 /// methods exercise:
 const square = {
     area: function (num) {
@@ -171,9 +171,9 @@ const square = {
     }
 }
 
+
 ////////////////////////////////////
 /// Egg Laying exercise:
-
 
 
 ///////////////////////////////////
@@ -184,35 +184,42 @@ const cleanNames = names.map(function (n) {
 
 });
 
-const firstNames = fullNames.map( function(n) {
-    return n.first;
-});
 
 /////////////////////////////////////
+/// ?:
+// const firstNames = fullNames.map( function(n) {
+//     return n.first;
+// });
+
+
+////////////////////////////////////
 /// Arrow Functions
 const greet = (n) => {
     return `Hey ${n}!`;
 }
 
+
 ///////////////////////////////////
-/// filter exercise:
+/// filter exercise #49:
 
-
-
-
-// //////////////////////////////////
 
 // alert("Document Object Model Son!...");
 
-// //////   Pickles Exercise:
-// document.querySelector('span');
-// // document.querySelector('span').innerText() = 'Disgusting';
 
-// ///////  Manipulating Attributes Exercise:
+//////////////////////////////////////
+/// Pickles Exercise #53:
+// document.querySelector('span');
+// document.querySelector('span').innerText() = 'Disgusting';
+
+
+//////////////////////////////////////
+/// Manipulating Attributes Exercise #54:
 // document.querySelector('img').src = 'https://www.flaticon.com/svg/static/icons/svg/3523/3523063.svg';
 // document.querySelector('img').alt = "chicken";
 
-// //////   Magical Forest Circle Exercise:
+
+//////////////////////////////////////
+/// Magical Forest Circle Exercise #55:
 // const container = document.querySelector('div');
 // const image = document.querySelector('img');
 
