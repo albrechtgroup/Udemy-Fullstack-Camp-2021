80717fadf804a574fcb39ad68232b73214fe4a10
Andy Albrecht <aalbrecht33733@gmail.com>
2022-02-15 19:53:00 -0500

    Document.querySelector/.querySelectorAll()

--- dom/app.js
+++ dom/app.js
@@ -1,6 +1,49 @@
 alert('Connected!');
-/////////////////////////////
+/// DOM elements:
 
+///
+console.dir(document);
+///
+
+///// MDN Docs - document.getElementById():
+// var element = document.getElementById(id);
+
+
+/// MDN Docs - document.getElementsByTagName():
+// var elements = document.getElementsByTagName(name);
+
+/// MDN Docs - document.getElementsByClassName():
+
+
+
+//////////////////////////////////////////
+/// New, all-in-one method to select a single
+/// element:
+/// querySelector()
+
+/// Finds first h1 element:
+document.querySelector('h1');
+
+/// Finds first element with ID of red:
+document.querySelector('#red');
+
+/// Finds first element with class class of:
+document.querySelector('.big');
+
+/// Finds an anchor tag with the title java:
+document.querySelector('a[title="java"]');
+
+
+///////////////////////////////////////////
+/// querySelectorAll():
+/// Same idea, but returns a collection of
+/// matching elements. Instead of 1st match.
+document.querySelectorAll('p');
+
+
+
+//////////////////////////////////////////
+/// POKEMON Html display thing:
 const container = document.querySelector('#container');
 const baseURL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
 
