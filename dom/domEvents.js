97e20b45a621029d53506f3e95322bed86018fbb
Andy Albrecht <aalbrecht33733@gmail.com>
2022-02-20 14:45:45 -0500

    Added CSS, Comments on js file

--- dom/domEvents.js
+++ dom/domEvents.js
@@ -1,34 +1,35 @@
-////// onclick Event
-// const btn = document.querySelector('#v2');
+//// onclick Event
+const btn = document.querySelector('#v2');
 
-// btn.onclick = function() {
-//     alert('OnClicked Mang!');
-// }
+btn.onclick = function() {
+    alert('OnClicked Mang!');
+}
 
-////// addEventListener *
-// const h1 = document.querySelector('h1');
 
-// h1.addEventListener('click', () => {
-//     alert("You clicked h1 Son.");
-// })
+//// addEventListener *
+const h1 = document.querySelector('h1');
 
-//////////////////////////////////////////
+h1.addEventListener('click', () => {
+    alert("You clicked h1 Son.");
+})
+
+
 ////  Keyboard Events & Event Objects
-// document.querySelector('button').addEventListener('click', function(evt) {
-//     console.log(evt);
-// })
+document.querySelector('button').addEventListener('click', function(evt) {
+    console.log(evt);
+})
 
-// const input = document.querySelector('input');
-// input.addEventListener('keydown', function() {
-//     console.log("Key Down!")
-// })
+const input = document.querySelector('input');
+input.addEventListener('keydown', function() {
+    console.log("Key Down!")
+})
 
-// input.addEventListener('keyup', function(evt) {
-//     console.log(evt)
-// })
+input.addEventListener('keyup', function(evt) {
+    console.log(evt)
+})
 
-//////////
 
+////
 window.addEventListener('keydown', function(evt) {
     console.log(evt.key)
     console.log(evt.code)
