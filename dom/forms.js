c188951f336412df25898d5f2812064884a93a27
Andy Albrecht <aalbrecht33733@gmail.com>
2022-02-24 10:12:41 -0500

    New comments added to form

--- dom/forms.js
+++ dom/forms.js
@@ -1,6 +1,8 @@
+/// Form events & .preventDefault():
+
 const postForm = document.querySelector('#postForm');
 const commentsContainer = document.querySelector('#comments');
-
+/// .preventDefault() added:
 postForm.addEventListener('submit', function(e) {
     e.preventDefault();
     // const usernameInput = document.querySelectorAll('input')[0];
@@ -8,11 +10,14 @@ postForm.addEventListener('submit', function(e) {
     const usernameInput = postForm.elements.username;
     const commentsInput = postForm.elements.comments;
     addComment(usernameInput.value, commentsInput.value);
+    /// Changes input values back to empty.
     usernameInput.value = '';
     commentsInput.value = '';
     
 });
 
+/// .append comments to a newly created 
+/// a bold tag list item. b, li
 const addComment = (username, comments) => {
     const newComment = document.createElement('li');
     const bTag = document.createElement('b');
@@ -22,7 +27,7 @@ const addComment = (username, comments) => {
     commentsContainer.append(newComment);
 }
 
-/////////////////////////////////////
+
 const input = document.querySelector('input');
 const h1 = document.querySelector('h1');
 
@@ -30,7 +35,8 @@ input.addEventListener('change', function(e) {
     console.log("Change!");
 })
 
-
+/// Changes the innerText of the h1 to the
+/// value of the input content.
 input.addEventListener('input', function(e) {
     h1.innerText = input.value;
 })
