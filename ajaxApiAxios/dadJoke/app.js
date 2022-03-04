5aaa27d6a3df44eae52c42a8afb81a3840d29aff
Andy Albrecht <aalbrecht33733@gmail.com>
2022-03-04 15:04:59 -0500

    Folder Rename

---
+++ ajaxApiAxios/dadJoke/app.js
@@ -0,0 +1,24 @@
+const jokes = document.querySelector('#jokes');
+const button = document.querySelector('button');
+
+const addNewJoke = async () => {
+    const jokeText = await getDadJoke();
+    console.log(jokeText)
+    const newLI = document.createElement('LI');
+    newLI.append(jokeText);
+    jokes.append(newLI);
+}
+
+const getDadJoke = async () => {
+    try {
+        const config = { headers: { Accept: 'application/json' }}
+        const res = await axios.get('https://icanhazdadjoke.com/', config)
+        return res.data.joke;
+    } catch (e) {
+        return "Sorry, No jokes available."
+    }
+    
+}
+
+button.addEventListener('click', addNewJoke);
+
