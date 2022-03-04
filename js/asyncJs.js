d3233b7b94da7b8ad62a88599f0e4d30126730ef
Andy Albrecht <aalbrecht33733@gmail.com>
2022-03-04 15:03:21 -0500

    The call stack, promises, Async & Await keywords

--- js/asyncJs.js
+++ js/asyncJs.js
@@ -81,7 +81,7 @@ fakeRequestPromise('yelp.com/api/coffee/page1')
         return fakeRequestPromise('yelp.com/api/coffee/page3')
     })
     .then(() => {
-        console.log("It Worked!!! (Page3)")
+        console.log("It Worked Again!!! (Page3)")
     })
     .catch(() => {
         console.log("OH NO, A Request Failed!!!")
@@ -90,18 +90,53 @@ fakeRequestPromise('yelp.com/api/coffee/page1')
 
 /// Creating our own Promises:
 const fakeRequest = (url) => {
-    new Promise((resolve, reject) => {
+    return new Promise((resolve, reject) => {
+        const rand = Math.random();
         setTimeout(() => {
-            resolve();
-        }, 1500)
+            if(rand < 0.6) {
+                resolve("More Fake News here.");
+            }
+            reject("Request DENIED!*");
+        }, 2500)
     })
 }  
 
+fakeRequest('/dogs/1')
+    .then(() => {
+        console.log("Done with Request*!*!")
+        console.log("Data is:", data)
+    })
+    .catch((err) => {
+        console.log("OH NOOOO!", err)
+    })
 
-/// The 'Async' keyword:
+
+/// Async Functions - A new and cleaner syntax
+/// for working with async code!
+/// The 'Async' keyword: Automatically
+/// returns a promise.
+const login = async (username, password) => {
+    if (!username || !password) throw 'Missing Credentials'
+    if (password === 'assMan33') return '*** Welcome! ***'
+    throw 'Invalid Password.'
+}
+
+login('booker', 'assMan33')
+    .then(msg => {
+        console.log("*** Loggin IN! ***")
+        console.log(msg)
+    }) 
+    .catch(err => {
+        console.log("!!! LOGIN Error !!!")
+        console.log(err)
+    })
 
 
 /// The 'Await' keyword:
+/// Await will pause the execution of the 
+/// function, waiting for a promise to be 
+// resolved.
+
 
 
 /// Handling errors in Async functions:
