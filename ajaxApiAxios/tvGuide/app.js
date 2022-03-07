cfa55ea966071e2092613ea79911bfad9a9a8c6c
Andy Albrecht <aalbrecht33733@gmail.com>
2022-03-07 14:26:37 -0500

    TVGuide app to search and return TvMaze title's and photo's

--- ajaxApiAxios/tvGuide/app.js
+++ ajaxApiAxios/tvGuide/app.js
@@ -3,10 +3,17 @@ form.addEventListener('submit', async function (e) {
     e.preventDefault();
     const searchTerm = form.elements.query.value;
     const res = await axios.get(`http://api.tvmaze.com/search/shows?q=${searchTerm}`);
-    const img = document.createElement('IMG');
-    img.src = res.data[0].show.image.medium;
-    document.body.append(img)
+    makeImages(res.data)
+    form.elements.query.value = '';
 })
 
-
+const makeImages = (shows) => {
+    for (let result of shows) {
+        if (result.show.image) {
+            const img = document.createElement('IMG');
+            img.src = result.show.image.medium;
+            document.body.append(img)
+        }
+    }
+}
 
