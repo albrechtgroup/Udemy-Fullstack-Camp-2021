48acc1203c4a37df2f6a9f76eb078731b87188df
Andy Albrecht <aalbrecht33733@gmail.com>
2022-03-18 15:07:53 -0400

    Basic ExpressJS skeleton app, Hello World example from Docs

--- express/expressBasic/index.js
+++ express/expressBasic/index.js
@@ -1,9 +1,3 @@
-// const express = require('express');
-
-
-
-
-
 /// ExpressJS Docs 'Hello World!' Example:
 const express = require('express')
 const app = express()
@@ -11,15 +5,6 @@ const port = 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
-})
-
-/// I've added /cats and /dogs routes.
-app.get('/cats', (req, res) => {
-    res.send('Meoww!!!')
-})
-
-app.get('/dogs', (req, res) => {
-    res.send('Woof Woof!!')
 })
 
 
