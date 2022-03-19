e194acb986354e3c032d390add6e0a1f7ce9e351
Andy Albrecht <aalbrecht33733@gmail.com>
2022-03-18 20:47:22 -0400

    Basic ExpressJS skeleton app, Hello World example from Docs

--- express/expressApp/index.js
+++ express/expressApp/index.js
@@ -2,18 +2,6 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-//////////////////////////////////////////
-// const num = Math.floor(Math.random() * 10) + 1;
-// const port = 3000
-
-// console.dir(app)
-
-// app.use((req, res) => {
-//     console.log("Express Server Loaded!!");
-//     // res.send("Request Recieved: This is Response.")
-//     res.send('<h1>Adding an H1 Sir.</h1>');
-// })
-
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use(express.urlencoded({ extended: true }));
@@ -21,18 +9,11 @@ app.use(express.json())
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs');
 
-
-
-///// "Root Route"  home.ejs
-app.get('/', (req, res) => {
-    res.render('home');
-})
-
-///////////////////////////////////
-// 'Subreddit' Route. Express Path Parameters
+/// 'Subreddit' Route. Express Path Parameters
 app.get('/r/:subreddit', (req, res) => {
-    res.send("THIS is a SubReddit!")
     console.log(req.params)
+    const { subreddit } = req.params;
+    res.send(`<h1>You are browsing the ${subreddit} Subreddit Parameter.</h1>`) 
 })
 
 app.get("/r/:subredditName", function(req, res){
@@ -44,8 +25,15 @@ app.get("/r/:subredditName/comments/:id/:title/", function(req, res){
     res.send("WELCOME TO THE COMMENTS PAGE!");
 });
 
-////////////////////////////////////
-//// /cats /dogs
+
+
+//////////// Another Example //////////////
+/// "Root Route"  home.ejs that's connected.
+app.get('/', (req, res) => {
+    res.render('home');
+})
+
+/// /cats /dogs routes
 app.get('/cats', (req, res) => {
     console.log("CAT Request/Rte.")
     res.send("MEOWWW!")
@@ -55,12 +43,15 @@ app.get('/dogs', (req, res) => {
     res.send("WOOF WOOF ! !")
 })
 
-////////////  'catch-all' get request. *last
+/// * = 'catch-all' get request. 
+/// Comes before all other routes so
+/// put last.
 app.get('*', (req, res) => {
     res.send("NO PATH! NO PATH!!!")
 })
 
 
+/// app.listen for Subreddit & other example.
 app.listen(3000, () => {
     console.log("LISTENING on PORT 3000*");
 })
